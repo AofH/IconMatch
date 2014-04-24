c8767258cf9ef5ff63325fdb5ddb93f5b61cd281
William Ridder <william@ridder.ca>
2014-04-23 20:11:49 -0400

    Made the timer animation smoother

--- Timer.js
+++ Timer.js
@@ -2,28 +2,38 @@ function Timer(x,y,width,height,timeLength){
 	this.x = x;
 	this.y = y;
 	this.width = width;
-	this.storedWidth = width;
+	//this.storedWidth = width;
 	this.height = height;
-	this.timeLength = timeLength*2;
+	this.timeLength = timeLength;
 	this.timerColor = YELLOW;
 	this.timeRemaining = this.timeLength;
 
-	this.pixelsChangePerSecond = this.width/(this.timeLength);
+	this.pixelsChangePerMilliSecond = this.width/(this.timeLength*1000);
 
 	this.lastAnimationFrameTime = 0;
 	this.lastFpsUpdateTime = 0;
 }
 
 Timer.prototype.update = function(now){
-	var fps = 1000/(now - this.lastAnimationFrameTime);
-	var difference = now - this.lastAnimationFrameTime;
-	this.lastAnimationFrameTime = now;
 	
-	if (now - this.lastFpsUpdateTime > 500) {
+	//var fps = 1000/(now - this.lastAnimationFrameTime);
+	
+	var difference = now - this.lastAnimationFrameTime;
+
+
+	if(this.pixelsChangePerMilliSecond * difference > 1)
+	{
+		this.lastAnimationFrameTime = now;
+		this.width -= (this.pixelsChangePerMilliSecond * difference);
+		//prevent the width from going below 0 and displaying wierdly
+		if (this.width < 0) {
+			this.width = 0;
+		}
+	}
+	//Update the timer by 1 second;
+	if (now - this.lastFpsUpdateTime > 1000) {
 	  this.timeRemaining--;
       this.lastFpsUpdateTime = now;
-      this.width -= this.pixelsChangePerSecond;	  
-      this.count++;
     }
-   	return fps; 
+   	//return fps; 
 }
